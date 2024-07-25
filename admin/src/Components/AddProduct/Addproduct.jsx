c0c77ad6f47792bbe780c4f8f14f2b16344af532
Sathwikg04 <117715188+Sathwikg04@users.noreply.github.com>
2024-07-26 00:56:41 +0530

    day-10

--- admin/src/Components/AddProduct/Addproduct.jsx
+++ admin/src/Components/AddProduct/Addproduct.jsx
@@ -4,29 +4,72 @@ import upload_area from '../../assets/main.jpg'
 
 const Addproduct = () => {
     const[image,setImage] = useState(false);
+    const [productDetails,setProductDetails] = useState({
+      name:"",
+      image:"",
+      category:"women",
+      new_price:"",
+      old_price:""
+    });
 
     const imageHandler = (e) => {
         setImage(e.target.files[0]);
     }
+    const changeHandler = (e) => {
+      setProductDetails({...productDetails, [e.target.name]:e.target.value})
+    }
+    const Add_Product = async() => {
+      console.log(productDetails);
+      let responseData;
+      let product = productDetails;
+
+      let formData = new FormData();
+      formData.append('product', image);
+
+      await fetch('http://localhost:4000/upload', {
+        method:'POST',
+        header:{
+          Accept:'application/json',
+        },
+        body:formData,
+      }).then((resp) => resp.json()).then((data) => {responseData=data})
+
+      if(responseData.success)
+      {
+        product.image = responseData.image_url;
+        console.log(product);
+        await fetch('http://localhost:4000/addproduct',{
+          method:'POST',
+          headers:{
+            Accept:'application/json',
+            'Content-Type':'application/json',
+          },
+          body:JSON.stringify(product),
+        }).then((resp)=>resp.json()).then((data)=>{
+          data.success?alert("Product Added"):alert("Failed")
+        })
+      }
+    }
+
   return (
     <div className='add-product'>
       <div className="addproduct-itemfield">
         <p>Product Title</p>
-        <input type="text" name='name' placeholder='Type here' />
+        <input value={productDetails.name} onChange={changeHandler} type="text" name='name' placeholder='Type here' />
       </div>
       <div className="addproduct-price">
         <div className="addproduct-itemfield">
             <p>Price</p>
-            <input type="text" name='old_price' placeholder='Type here'/>
+            <input value={productDetails.old_price} onChange={changeHandler} type="text" name='old_price' placeholder='Type here'/>
         </div>
         <div className="addproduct-itemfield">
             <p>Offer Price</p>
-            <input type="text" name='new_price' placeholder='Type here'/>
+            <input value={productDetails.new_price} onChange={changeHandler} type="text" name='new_price' placeholder='Type here'/>
         </div>
       </div>
       <div className="addproduct-itemfield">
         <p>Product Category</p>
-        <select name="category" className='add-product-selector'>
+        <select value={productDetails.category} onChange={changeHandler} name="category" className='add-product-selector'>
             <option value="women">Women</option>
             <option value="men">Men</option>
             <option value="kid">Kid</option>
@@ -38,7 +81,7 @@ const Addproduct = () => {
         </label>
         <input onChange={imageHandler} type="file" name='image' id='file-input' hidden/>
       </div>
-      <button className='addproduct-btn'>ADD</button>
+      <button onClick={() => {Add_Product()}} className='addproduct-btn'>ADD</button>
     </div>
   )
 }
--- frontend/src/Pages/login.jsx
+++ frontend/src/Pages/login.jsx
@@ -1,22 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CSS/Login.css'
 
 const Login = () => {
+  const [state, setState] = useState("Login");
+  const [formData,setFormData] = useState({
+    username:"",
+    password:"",
+    email:""
+  })
+
+  const changeHandler = (e) => {
+    setFormData({...formData,[e.target.name]:e.target.value})
+  }
+
+  const login = async () => {
+    console.log(formData);
+  }
+  const signup = async () => {
+    console.log(formData);
+  }
+
   return (
     <div className='login'>
       <div className="login-container">
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
         <div className="login-fields">
-          <input type="text" placeholder='Your Name' />
-          <input type='email' placeholder='Email Address'/>
-          <input type='password' placeholder='Password'/>
+          {state === "Sign Up" ? <input name='username' value={formData.username} onChange={changeHandler} type="text" placeholder='Your Name' /> : <></>}
+          <input name='email' value={formData.email} onChange={changeHandler} type='email' placeholder='Email Address' />
+          <input name='password' value={formData.password} onChange={changeHandler} type='password' placeholder='Password' />
         </div>
-        <button>Continue</button>
-        <p className="login-login">
-          Already have an account? <span>Login here!</span>
-        </p>
+        <button onClick={()=>{state==="Login"?login():signup()}}>Continue</button>
+        {state === "Sign Up" ?
+          <p className="login-login">
+            Already have an account? <span onClick={()=>{setState("Login")}}>Login here!</span>
+          </p> :
+          <p className="login-login">
+            Create an account <span onClick={()=>{setState("Sign Up")}}>Click here!</span>
+          </p>}
         <div className="login-agree">
-          <input type='checkbox' name='' id=''/>
+          <input type='checkbox' name='' id='' />
           <p>BY continuing, I agree to the terms of use & privacy policy</p>
         </div>
       </div>
